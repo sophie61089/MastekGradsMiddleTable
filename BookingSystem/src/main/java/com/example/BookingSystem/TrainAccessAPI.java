256d65ee3af699a8e2456568b55cee36d6195f4c
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-21 14:34:06 +0000

    TrainJPARepository added

--- BookingSystem/src/main/java/com/example/BookingSystem/TrainAccessAPI.java
+++ BookingSystem/src/main/java/com/example/BookingSystem/TrainAccessAPI.java
@@ -1,6 +1,13 @@
 package com.example.BookingSystem;
 
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/trains/")
 public class TrainAccessAPI {
 
+	TrainJPARepository 
 	
 }
--- BookingSystem/src/main/java/com/example/BookingSystem/TrainJPARepository.java
+++ BookingSystem/src/main/java/com/example/BookingSystem/TrainJPARepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface TrainJPARepository extends CrudRepository<Train, Integer>{
 
+	
 }
