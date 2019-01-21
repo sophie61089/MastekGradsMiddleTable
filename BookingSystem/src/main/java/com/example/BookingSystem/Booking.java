f63ecb6a515bcd40535f8401b3ce7d43b43ea6f4
George61080 <44974891+George61080@users.noreply.github.com>
2019-01-21 14:22:22 +0000

    Booking

---
+++ BookingSystem/src/main/java/com/example/BookingSystem/Booking.java
@@ -0,0 +1,41 @@
+package com.example.BookingSystem;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name="Group_Booking")
+public class Booking {
+	
+	Integer bookingId;
+	String time;
+	String date;
+	
+	public Integer getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(Integer bookingId) {
+		this.bookingId = bookingId;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	@Override
+	public String toString() {
+		return "Booking [bookingId=" + bookingId + ", time=" + time + ", date=" + date + "]";
+	}
+	
+	
+	
+
+}
