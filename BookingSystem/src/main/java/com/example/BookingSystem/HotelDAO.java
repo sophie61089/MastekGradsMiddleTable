be6575d1dc4352a6934567b46ca8699151e846ee
joe61081 <44974861+joe61081@users.noreply.github.com>
2019-01-21 14:18:24 +0000

    HotelDAO Created

---
+++ BookingSystem/src/main/java/com/example/BookingSystem/HotelDAO.java
@@ -0,0 +1,17 @@
+package com.example.BookingSystem;
+
+import java.util.List;
+
+public class HotelDAO implements DataAccessObject<Hotel>{
+
+	@Override
+	public Hotel add(Hotel newEntity) {
+		return null;
+	}
+
+	@Override
+	public List<Hotel> find(int key) {
+		return null;
+	}
+
+}
