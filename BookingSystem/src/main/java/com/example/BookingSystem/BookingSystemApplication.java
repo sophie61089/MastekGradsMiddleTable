e5b3e078d81666df3cb41b503096077d57a89410
scottfreeman1996 <Scott61079@GB-LD1LPW10G008.mshome.net>
2019-01-21 15:19:08 +0000

    Fixing Ellie's mistakes

--- BookingSystem/src/main/java/com/example/BookingSystem/BookingSystemApplication.java
+++ BookingSystem/src/main/java/com/example/BookingSystem/BookingSystemApplication.java
@@ -9,10 +9,7 @@ public class BookingSystemApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(BookingSystemApplication.class, args);
-<<<<<<< HEAD
-=======
-	}
->>>>>>> branch 'master' of https://github.com/sophie61089/MastekGradsMiddleTable.git
+
 
 		
 		
