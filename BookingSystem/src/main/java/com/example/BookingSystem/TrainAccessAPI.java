342f5d0a0de3119213983e027b76b8084b947f02
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-21 14:36:36 +0000

    edited train jpa

--- BookingSystem/src/main/java/com/example/BookingSystem/TrainAccessAPI.java
+++ BookingSystem/src/main/java/com/example/BookingSystem/TrainAccessAPI.java
@@ -1,13 +1,31 @@
 package com.example.BookingSystem;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/trains/")
 public class TrainAccessAPI {
+ 
+	TrainJPARepository repository;
 
-	TrainJPARepository 
+	public TrainJPARepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(TrainJPARepository repository) {
+		this.repository = repository;
+	}
+	
+	@Path("/list")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})//JSON will be default format when fetching data, if not JSON will give XML
+	public Iterable<Train> listTrains(){
+		return getRepository().findAll();
+	}
 	
 }
