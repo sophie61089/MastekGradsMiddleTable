3787c99cf55c6fc340c1cba4cdbc337a97fb09fb
scottfreeman1996 <Scott61079@GB-LD1LPW10G008.mshome.net>
2019-01-21 14:38:00 +0000

    CustomerAPI and JPA Repository

---
+++ BookingSystem/src/main/java/com/example/BookingSystem/CustomerAccessAPI.java
@@ -0,0 +1,47 @@
+package com.example.BookingSystem;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Path("/customer/")
+public class CustomerAccessAPI {
+	
+	CustomerJPARepository repository;
+
+	public CustomerJPARepository getRepository() {
+		return repository;
+	}
+
+	@Autowired
+	public void setRepository(CustomerJPARepository repository) {
+		this.repository = repository;
+	}
+	
+	// Url: http://localhost:9900/products/list
+	@Path("/list")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Iterable<Customer> listCustomers(){
+		return getRepository().findAll();	
+	}
+	
+	@POST
+	@Path("/register")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+	public Customer addCustomer(@BeanParam Customer newCustomer) {
+		getRepository().save(newCustomer);
+		return newCustomer;
+	}
+
+}
