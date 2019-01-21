ac0330a3ba3eba7dd7b672186784af2c7cd41caa
scottfreeman1996 <Scott61079@GB-LD1LPW10G008.mshome.net>
2019-01-21 14:41:20 +0000

    ctx

--- BookingSystem/src/main/java/com/example/BookingSystem/BookingSystemApplication.java
+++ BookingSystem/src/main/java/com/example/BookingSystem/BookingSystemApplication.java
@@ -2,12 +2,13 @@ package com.example.BookingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BookingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingSystemApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(BookingSystemApplication.class, args);
 	}
 
 }
