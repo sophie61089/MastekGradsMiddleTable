a511482a5eff44865783d43a12a030db365a9afa
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-21 14:45:25 +0000

    added ID for train

--- BookingSystem/src/main/java/com/example/BookingSystem/Train.java
+++ BookingSystem/src/main/java/com/example/BookingSystem/Train.java
@@ -1,6 +1,9 @@
 package com.example.BookingSystem;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +24,8 @@ public class Train {
 				+ ", totalCarriageNo=" + totalCarriageNo + ", driver=" + driver + ", provider=" + provider + ", price="
 				+ price + "]";
 	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	public int getTrainNo() {
 		return trainNo;
 	}
