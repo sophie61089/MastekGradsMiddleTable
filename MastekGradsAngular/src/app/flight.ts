d337f211687e4c8057642ed5bcd8b401ef7576f9
61073 <mike.wood@mastek.com>
2019-01-22 14:24:45 +0000

    Added flight interface

--- MastekGradsAngular/src/app/flight.ts
+++ MastekGradsAngular/src/app/flight.ts
@@ -1,2 +1,8 @@
 export interface Flight {
+    departureDate:string;
+    arrivalDate:string;
+    flightOrigin:string;
+    flightDestination:string;
+    airline:string;
+    flightPrice:number;
 }
