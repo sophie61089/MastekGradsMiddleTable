06f96f21993f741369235af43472c1691348b591
61073 <mike.wood@mastek.com>
2019-01-22 14:33:59 +0000

    Added flight service

--- MastekGradsAngular/src/app/flight.service.ts
+++ MastekGradsAngular/src/app/flight.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Flight } from './flight';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
+  rootURL: string;
+  rootTrainingURL:string;
 
-  constructor() { }
+  constructor(private httpsvc: HttpClient) {
+    this.rootURL = "http://localhost:9900/flight";
+  }
+
+  getFlights(): Observable<Flight[]> {
+    return this.httpsvc.get<Flight[]>(this.rootURL + "/list")
+  }
+
+  addNewFlight(newFlight: Flight): Observable<Flight> {
+
+    const httpOpts = {
+      headers: new HttpHeaders(
+        {
+          'Content-Type':
+            'application/x-www-form-urlencoded;charset=UTF-8'
+        })
+    }
+    var reqBody = "flightNo=" + newFlight.flightNo + "&departureDate=" + newFlight.departureDate 
+    + "&arrivalDate=" + newFlight.arrivalDate + "&flightOrigin=" + newFlight.flightOrigin
+    + "&flightDestination=" + newFlight.flightDestination + "&departureDate=" + newFlight.airline
+    + "&flightPrice=" + newFlight.flightPrice
+
+    return this.httpsvc.post<Flight>(
+      this.rootURL + "/register", reqBody, httpOpts)
+  }
 }
+
