a6fdde4ebda7bbacb4f421f61847ef92b3beca9b
sophie61089 <44974945+sophie61089@users.noreply.github.com>
2019-01-22 14:14:12 +0000

    nEw cOmPoNeNtS

--- MastekGradsAngular/src/app/app.module.ts
+++ MastekGradsAngular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { BookingComponent } from './booking/booking.component';
+import { CustomerComponent } from './customer/customer.component';
+import { FlightComponent } from './flight/flight.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { TrainComponent } from './train/train.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    BookingComponent,
+    CustomerComponent,
+    FlightComponent,
+    HotelComponent,
+    TrainComponent
   ],
   imports: [
     BrowserModule,
