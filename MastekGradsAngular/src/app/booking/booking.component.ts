23b44cf5fcd4b3fb0e33cc1af67fa4ff0d15f324
sophie61089 <44974945+sophie61089@users.noreply.github.com>
2019-01-23 10:52:49 +0000

    edit

--- MastekGradsAngular/src/app/booking/booking.component.ts
+++ MastekGradsAngular/src/app/booking/booking.component.ts
@@ -32,35 +32,3 @@ export class BookingComponent implements OnInit {
     )
   }
 }
-  constructor(private bookService:BookingsService) { 
-    this.bookings=[]
-  }
-
-  addNewBooking(newBooking:Booking){
-    this.bookService.addNewBooking(newBooking).subscribe(
-      res=>{
-        this.bookService.getBookings().subscribe(
-          res =>{this.bookings =res}
-        )
-      }
-    )
-  }
-
-  deleteBooking(bookingId:number){
-    this.bookService.deleteBooking(bookingId).subscribe(
-      res=>{
-        this.bookService.getBookings().subscribe(
-          res =>{ this.bookings =res}
-        )
-      }
-    )
-  }
-
-
-  ngOnInit() {
-    
-  this.bookService.getBookings().subscribe(
-    res =>(this.bookings= res)
-  )
-  }
-}
